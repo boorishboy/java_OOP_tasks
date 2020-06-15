b1b848242a9186132fe1e6ed4c37eb803bdda7a8
boorishboy <wojciech.chamski@gmail.com>
2020-06-15 12:56:27 +0200

    Task 3&4

--- src/pl/wsb/human/Human.java
+++ src/pl/wsb/human/Human.java
@@ -7,9 +7,27 @@ public class Human {
     Car car;
     Animal pet;
 
+    public Double salary;
+
     public Human(String name, Car car, Animal pet) {
         this.name = name;
         this.car = car;
         this.pet = pet;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if(salary < 0) {
+            throw new IllegalArgumentException("Hey what's wrong with you salary cannot be negative");
+        }
+
+        System.out.println("New data was sent to accounting system");
+        System.out.println("It's necessary to take annex to agreement of Ms. Joanna from the office");
+        System.out.println("Healthy Insurence and Tax Office got to know of salary change and there is no use to hide your real income");
+
+        this.salary = salary;
     }
 }
